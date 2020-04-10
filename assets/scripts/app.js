505f20f98fde2e7305294bd0a7d635dd3b253689
Matthew Laszcz <matthewlaszcz@gmail.com>
2020-04-09 20:28:39 -0400

    Code Refactored.
    Improved load/run time after analyzing the performance Call Tree sequence.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,42 +2,65 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+let logEntries = [];
+
 function getUserInput (){
     return parseInt(userInput.value);
 }
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-outputResult(currentResult, calcDescription);
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function writeToLog (
+    operationIdentifier, 
+    prevResult, 
+    operationNumber, 
+    newResult)
+    {
+        const logEntry = {
+            operation: operationIdentifier,
+            previousResult: prevResult,
+            number: operationNumber,
+            result: newResult
+        }; //Creating an object.
+        logEntries.push(logEntry);
+        console.log(logEntries);
+
 }
 
 function add() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber; //.value will give the actual value from userInput from vendorJS
+    currentResult += enteredNumber; //.value will give the actual value from userInput from vendorJS
     createAndWriteOutput('+',initialResult,enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
 
 }
 
 function subtract(){
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult - parseInt(enteredNumber); //.value will give the actual value from userInput from vendorJS
+    currentResult -= parseInt(enteredNumber); //.value will give the actual value from userInput from vendorJS
     createAndWriteOutput('-',initialResult,enteredNumber); 
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply () {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult * parseInt(enteredNumber); //.value will give the actual value from userInput from vendorJS
+    currentResult *= parseInt(enteredNumber); //.value will give the actual value from userInput from vendorJS
     createAndWriteOutput('*',initialResult,enteredNumber); 
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide () {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult / parseInt(enteredNumber); //.value will give the actual value from userInput from vendorJS
+    currentResult /= parseInt(enteredNumber); //.value will give the actual value from userInput from vendorJS
     createAndWriteOutput('/',initialResult,enteredNumber); 
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 }
 
 
@@ -46,7 +69,6 @@ addBtn.addEventListener('click', add); //only pass the function name do noot use
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
- // code can be injected with ${defaultResult} this will show the value of default result
 
 
 
